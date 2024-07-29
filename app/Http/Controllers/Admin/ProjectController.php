761f3de7fc9184d835dd285306d1f4e6638daf02
Luca Sammarco <luca.sammarco7@gmail.com>
2024-07-29 17:32:28 +0200

    added edit update delete

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -38,7 +38,18 @@ class ProjectController extends Controller
         //
         // dd($request->all());
 
-        $data =$request->all();
+        $data = $request->validate([
+            'name' => 'required|string|max:255|unique:projects',
+            'description' => 'required|string',
+            'url' => 'required|url',
+            'programming_language' => 'required|string|max:255'
+        ]);
+
+
+
+
+
+
         $data["author"] = Auth::user()->name;
         $data["updated_on"] = Carbon::now();
 
@@ -60,24 +71,42 @@ class ProjectController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Project $project)
     {
         //
+        return view('admin.projects.edit', compact('project'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Project $project)
     {
         //
+
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'url' => 'required|url',
+            'programming_language' => 'required|string|max:255'
+        ]);
+
+        $project->update($data);
+
+        return redirect()->route('admin.projects.show', $project);
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Project $project)
     {
         //
+
+        $project->delete();
+
+        return redirect()->route('admin.projects.index');
+
     }
 }
